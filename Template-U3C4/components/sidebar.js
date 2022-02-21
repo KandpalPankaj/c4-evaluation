2fb2f144b1471ed852c31fde91acbbeaef2b87fa
pankajnandi <97446286+pankajnandi@users.noreply.github.com>
2022-02-21 16:25:39 +0530

    Delete sidebar.js

--- Template-U3C4/components/sidebar.js
+++
@@ -1,8 +0,0 @@
-function sidebar() {
-
-
-    // return your html component here
-    //Make sure to give input search box id as ""
-    return`<input type="text" id="searchbar" placeholder="Search"></input>`
-}
-export default sidebar
